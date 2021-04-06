5bde7a5f8438c0a2a230639785059f0ce0ec89b9
mcidoni <matthewcidoni@gmail.com>
2021-04-06 17:55:33 -0400

    init commit

---
+++ etch-a-sketch.js
@@ -0,0 +1,36 @@
+console.log('loaded, buddy');
+// select elements on page, canvas, button, etc
+const canvas = document.querySelector('#etch-a-sketch');
+
+const context = canvas.getContext('2d');
+
+const shakeButton = document.querySelector('.shake');
+
+
+// setup html canvas
+
+// const { width } = canvas;
+// const { height } = canvas;
+
+const { width, height } = canvas;
+
+console.log(width, height);
+
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 10;
+
+// start draw
+context.beginPath();
+context.moveTo(200, 200);
+context.lineTo(200, 200);
+
+context.stroke();
+
+// draw function
+
+// key handler
+
+// clear (shake) function
+
+// event listener for arrow keys
